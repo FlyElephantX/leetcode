fef89852a277ab47e5cb7a3428be8443b333cce6
FlyElephant <>
2020-07-20 23:57:37 +0800

    283. Move Zeroes update

--- 0283-Move Zeroes/src/algorithm/Test.java
+++ 0283-Move Zeroes/src/algorithm/Test.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 class Solution {
     public void moveZeroes(int[] nums) {
         for (int i = 0, j = 0; i < nums.length; i++) {
-            if (nums[i] > 0) {
-                swap(nums, i++, j++);
+            if (nums[i] != 0) {
+                swap(nums, i, j++);
             }
         }
     }
@@ -44,7 +44,7 @@ class Solution {
 public class Test {
 
     public static void main(String[] args) {
-        int[] nums = {1,0,1};
+        int[] nums = {0,1,0,3,12};
         Solution solution = new Solution();
         solution.moveZeroes(nums);
         System.out.println("移动结果:" + Arrays.toString(nums));
