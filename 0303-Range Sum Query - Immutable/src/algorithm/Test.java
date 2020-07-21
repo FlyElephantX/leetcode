2876ae1a298da2df01857cc80b4dd680a4625c9d
FlyElephant <>
2020-07-21 21:52:34 +0800

    303. Range Sum Query - Immutable

---
+++ 0303-Range Sum Query - Immutable/src/algorithm/Test.java
@@ -0,0 +1,27 @@
+package algorithm;
+
+class NumArray {
+
+    private int[] sums;
+    public NumArray(int[] nums) {
+        sums = new int[nums.length + 1];
+        for (int i = 0; i < nums.length; i++) {
+            sums[i + 1] = sums[i] + nums[i];
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        return sums[j + 1] - sums[i];
+    }
+}
+
+
+public class Test {
+
+    public static void main(String[] args) {
+        int[] nums = {-2, 0, 3, -5, 2, -1};
+        NumArray numArray = new NumArray(nums);
+        int res = numArray.sumRange(0, 2);
+        System.out.println("数组结果:" + res);
+    }
+}
